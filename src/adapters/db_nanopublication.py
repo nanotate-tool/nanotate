f3f1efa7a2fa8e20a2e627061d304e9c4ff75e6d
Jose Miguel Ruano <miguel199308@gmail.com>
2020-10-15 21:45:21 -0500

    fixed bug on the rdf generation for nanopub from database

--- src/adapters/db_nanopublication.py
+++ src/adapters/db_nanopublication.py
@@ -12,14 +12,34 @@ class DBNanopublication(GraphNanopublication):
     fue almacenada en una base de datos
     """
 
+    dateTimeFormat = "%Y-%m-%dT%H:%M:%S.%f%z"
+
     def __init__(self, nanopub: Nanopublication):
         self.nanopub = nanopub
+        created = (
+            self.nanopub.generatedAtTime
+            if self.nanopub.generatedAtTime != None
+            else self.nanopub.created_at.strftime(self.dateTimeFormat)
+        )
         super().__init__(
             url=self.nanopub.protocol.uri,
-            created=self.nanopub.created_at,
+            created=created,
             author=self.nanopub.author,
         )
         self.__computeAssertion()
+
+    def _initProvenance(self):
+        """ inicializa la cabezera ':provenance' de la nanopublicacion"""
+        super()._initProvenance()
+        # insertando components
+        for component in self.nanopub.components:
+            self.provenance.add(
+                (
+                    self.np.assertion,
+                    GraphNanopublication.PROV.wasDerivedFrom,
+                    rdflib.URIRef("https://hypothes.is/a/" + component.id),
+                )
+            )
 
     def __computeAssertion(self):
         if self.assertion != None:
